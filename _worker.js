849df303c1e34344b874666db5d005f7ed2dbe7f
lillinlin <126753730+lillinlin@users.noreply.github.com>
2025-08-28 23:36:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://github.com/ACL4SSR/ACL4SSR/raw/refs/heads/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/refs/heads/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
@@ -827,3 +827,4 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 	}
 
 }
+
